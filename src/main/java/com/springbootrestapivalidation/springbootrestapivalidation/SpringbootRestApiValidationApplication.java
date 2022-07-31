92986832de83b11911b7d8a8847a57b7869a9cf2
Manoj Seth <manojseth55@gmail.com>
2022-07-31 11:07:06 +0530

    initial commit

---
+++ src/main/java/com/springbootrestapivalidation/springbootrestapivalidation/SpringbootRestApiValidationApplication.java
@@ -0,0 +1,13 @@
+package com.springbootrestapivalidation.springbootrestapivalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApiValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestApiValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootrestapivalidation/springbootrestapivalidation/SpringbootRestApiValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootrestapivalidation.springbootrestapivalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
